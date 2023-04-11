c8924f74718f2d91d8dfdc791c14cb2564c15f80
valen2345 <130427365+valen2345@users.noreply.github.com>
2023-04-10 23:20:28 -0500

    Add files via upload

---
+++ potafolio.js
@@ -0,0 +1,8 @@
+function mostrardiv() {
+  var midiv = document.getElementById("about");
+  if (midiv.style.display === "none") {
+    midiv.style.display = "block";
+  } else {
+    midiv.style.display = "none";
+  }
+}
